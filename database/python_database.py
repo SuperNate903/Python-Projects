084226c6a7a8edf1d053770c9afca19fb7b7e144
SuperNate903 <minecraft.nate04@outlook.com>
2022-11-15 18:16:49 -0800

    Added a python database

---
+++ database/python_database.py
@@ -0,0 +1,25 @@
+import sqlite3
+
+fileList = ("information.docx","Hello.txt","myImage.png", \
+            "myMovie.mpg","World.txt","data.pdf","myPhoto.jpg")
+
+conn = sqlite3.connect("py_database.db")
+
+with conn:
+    cur = conn.cursor()
+    cur.execute("DROP TABLE IF EXISTS tbl_files")
+    cur.execute("CREATE TABLE IF NOT EXISTS tbl_files(\
+        ID INTEGER PRIMARY KEY AUTOINCREMENT, \
+        col_file TEXT \
+    )")
+    
+    for file in fileList:
+        if ".txt" in file:
+            cur.execute("INSERT INTO tbl_files(col_file) VALUES (?)", \
+                (file,))
+    
+    cur.execute("SELECT * FROM tbl_files")
+    varFile = cur.fetchall()
+    for item in varFile:
+        msg = "File: {}\n".format(item)
+        print(msg)
