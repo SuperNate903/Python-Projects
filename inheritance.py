72069e5a4418a23ae7593d637da03036d1ef500d
SuperNate903 <minecraft.nate04@outlook.com>
2022-11-16 16:04:57 -0800

    Added inheritance class assignment

---
+++ inheritance.py
@@ -0,0 +1,10 @@
+class Party:
+    attendees = 18
+    food_supply = 49
+    drink_supply = 58
+class Event_Organizer(Party):
+    fname = "John"
+    lname = "Almond"
+class Details(Party):
+    location = "1234 56th ST"
+    start_time = "6pm"
